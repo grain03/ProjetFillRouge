b7b272b66fdf534d0a7018f6f6a936d7b67d332f
Reda Grain <grain.reda.solicode@gmail.com>
2024-01-31 16:21:25 +0100

    add admin lte via npm

--- Branch-Fonctionnelle/3.Conception/Maqqutage/resources/views/layouts/layout.blade.php
+++ Branch-Fonctionnelle/3.Conception/Maqqutage/resources/views/layouts/layout.blade.php
@@ -17,7 +17,5 @@
         </div>
     </body>
 </html>
-    
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
